3df6fda8634797e59f6e06a478ab3886f0f9d985
Shimin051 <112827333+Shimin051@users.noreply.github.com>
2025-12-01 20:12:20 +0200

    Remove unnecessary comment from client.js

--- fyp/inngest/client.js
+++ fyp/inngest/client.js
@@ -1,5 +1,3 @@
-// /inngest/client.js
 import { Inngest } from "inngest";
-
 // The id must be stable and match wherever you send events from.
 export const inngest = new Inngest({ id: "fyp-app" });
